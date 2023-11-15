cc71143eb91d149c759bbce9f9bc7facd171bf43
lexborelli <alexasborelli@gmail.com>
2023-11-15 17:09:18 -0500

    corrected issues with showPage function & item display

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
-
+const studentList = document.querySelector('.student-list');
+const itemsPerPage = 9; 
 
 /*
 Create the `showPage` function
@@ -12,27 +13,26 @@ function showPage(list, page) {
    const start = (page * itemsPerPage) - itemsPerPage;
    const end = (page * itemsPerPage);
 
-   let studentList = document.querySelector('.student-list');
     studentList.innerHTML = "";
    
-   for( let i = 0; i < list.length; itt) {
-      if(i >= start && i <= end) {
+   for (let i = 0; i < list.length; i++) {
+      if (i >= start && i < end) {
          const html = `
          <li class="student-item cf">
          <div class="student-details">
-         <img class="avatar" src="${list[i].picture}" alt="photo of ${list[i].name}">
+         <img class="avatar" src="${list[i].picture.large}" alt="photo of ${list[i].name}">
          <h3>${list[i].name.first} ${list[i].name.last}</h3>
          <span class="email">${list[i].email}</span>
          </div>
          <div class="joined-details">
-         <span class="date">Joined ${items[i].registered.date}</span>
+         <span class="date">Joined ${list[i].registered.date}</span>
          </div>
          </li>
          `;
-         studentList.insertAdjacentHTML("beforehand",html);
+         studentList.insertAdjacentHTML('beforeend', html);
       }
    }
-};
+}
 
 /*
 Create the `addPagination` function
@@ -43,13 +43,13 @@ function addPagination(list) {
    const linkList = document.querySelector(".link-list"); 
    linkList.innerHTML = ""; 
 
-      for(let i = 1; i <= buttonsNeeded; itt) {
+      for (let i = 1; i <= buttonsNeeded; i++) {
          const html = `
          <li>
          <button type="button">${i}</button>
          </li>
          `;
-         linkList.insertAdjacentHTML("beforehand", html);
+         linkList.insertAdjacentHTML("beforeend", html);
       }
       linkList.querySelector("button").classList.add("active");
       
@@ -57,13 +57,15 @@ function addPagination(list) {
        const buttonClick = e.target.closest("button"); 
        const activeButton = linkList.querySelector('active');  
 
-         if(buttonClick) {
+         if (buttonClick) {
           activeButton.classList.remove('.active'); 
           buttonClick.classList.add('active');
-          showPage(list, page);
+          showPage(list, buttonclick.innerHTML);
          }
       });
-}
+};
 
 
 // Call functions
+addPagination(data); 
+showPage(data, 1);
